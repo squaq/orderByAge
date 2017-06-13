140dd96c1175c124179b448f2ec54e63bb7eb8a6
squaq <evertonsquaq@gmail.com>
2017-06-13 19:21:45 +0100

    one more fix

--- order.js
+++ order.js
@@ -1,6 +1,6 @@
 function order(arr){
     if(!arr || arr.length == 0) return 'empty Array';
-    if(typeof(arr[0].age) == 'undefined') return 'missing age value';
+    if(!arr[0].hasOwnProperty('age')) return 'missing age value';
     
     return arr.sort(function(obja, objb){return obja.age - objb.age});
 }
